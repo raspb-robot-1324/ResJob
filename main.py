26dd7a22d00f2c9d0105a0c8addf8815b22724e4
Raspberry Pi Pro <105301539+raspb-robot-1324@users.noreply.github.com>
2024-01-13 15:19:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,48 @@
-from openai import OpenAI
-import customtkinter as ctk
 import requests
 import json
 
-url = "https://linkedin-jobs-scraper-api.p.rapidapi.com/jobs"
 
-title = input("What job would you like? ")
-location = input("Where are you? ")
+def findjobs():
+    url = "https://linkedin-jobs-scraper-api.p.rapidapi.com/jobs"
 
-payload = {
-    "title": title,
-    "location": location,
-    "rows": 1
-}
-headers = {
-    "content-type": "application/json",
-    "X-RapidAPI-Key": "4addef27aemshcff167ca509dae0p11d1efjsn50d8839241b4",
-    "X-RapidAPI-Host": "linkedin-jobs-scraper-api.p.rapidapi.com"
-}
+    title = input("What job would you like? ")
+    location = input("Where are you? ")
 
-response = requests.post(url, json=payload, headers=headers)
+    payload = {
+        "title": title,
+        "location": location,
+        "rows": 1
+    }
+    headers = {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "4addef27aemshcff167ca509dae0p11d1efjsn50d8839241b4",
+        "X-RapidAPI-Host": "linkedin-jobs-scraper-api.p.rapidapi.com"
+    }
 
-allJobs = []
-a = json.loads(response.content)
-for job in a:
-    thisJob = "Title:", job["title"], "Company name:", job["companyName"], "Location:", job["location"], "Description", job["description"]
-    allJobs.append(thisJob)
+    response = requests.post(url, json=payload, headers=headers)
 
-for i in allJobs:
-    print(i)
+    allJobs = []
+    a = json.loads(response.content)
+    for job in a:
+        thisJob = "Title:", job["title"], "Company name:", job["companyName"], "Location:", job[
+            "location"], "Description", job["description"]
+        allJobs.append(thisJob)
 
-# Python program to create a basic GUI
-# application using the customtkinter module
+    for i in allJobs:
+        print(i)
+
 
 import customtkinter as ctk
-import tkinter as tk
 
-
-ctk.set_appearance_mode("System")
+ctk.set_appearance_mode("Light")
 
 ctk.set_default_color_theme("green")
 
 appWidth, appHeight = 660, 700
 
+job = False
 
-# App Class
+
 class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -68,27 +66,25 @@ class App(ctk.CTk):
 
         # choice Label
         self.choiceLabel = ctk.CTkLabel(self,
-                                     text="What type of job do you want?")
+                                        text="What type of job do you want?")
         self.choiceLabel.grid(row=1, column=0,
-                           padx=40, pady=40,
-                           sticky="ew")
+                              padx=40, pady=40,
+                              sticky="ew")
 
         # choice Entry Field
         self.choiceEntry = ctk.CTkEntry(self,
-                                     placeholder_text="Computer Engineer, Data scientist")
+                                        placeholder_text="Computer Engineer, Data scientist")
         self.choiceEntry.grid(row=1, column=1,
-                           columnspan=3, padx=40,
-                           pady=40, sticky="ew")
-
-
+                              columnspan=3, padx=40,
+                              pady=40, sticky="ew")
 
         # finalize the generation
         self.searchjobsButton = ctk.CTkButton(self,
-                                                   text="Search for Jobs")
+                                              text="Search for Jobs")
         self.searchjobsButton.grid(row=5, column=0,
-                                        columnspan=1,
-                                        padx=40, pady=40,
-                                        sticky="ew")
+                                   columnspan=1,
+                                   padx=40, pady=40,
+                                   sticky="ew")
 
         # results final yippeee!
         self.displayBox = ctk.CTkTextbox(self, width=600,
@@ -98,11 +94,10 @@ class App(ctk.CTk):
                              padx=40, pady=40, sticky="nsew")
 
 
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+from openai import OpenAI
+import customtkinter as ctk
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="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")
 
 ft = ""
 lt = ""
@@ -113,9 +108,7 @@ ep = ""
 hs = ""
 un = ""
 
-
 app = ctk.CTk()
-
 
 ftextbox = ctk.CTkTextbox(app)
 ftextbox.configure(state="normal")
@@ -188,34 +181,53 @@ label = ctk.CTkLabel(app, text="First Name", text_color="Black")
 label.configure(text="University:")
 label.place(relx=0.2, rely=0.7, anchor='s')
 
+patextbox = ctk.CTkTextbox(app)
+patextbox.configure(state="normal")
+patextbox = ctk.CTkTextbox(master=app, width=200, height=20)
+patextbox.place(relx=0.6, rely=0.8, anchor='s')
+patextbox = ctk.CTkTextbox(app, activate_scrollbars=False)
+palabel = ctk.CTkLabel(app, text="First Name", text_color="Black")
+palabel.configure(text="Past jobs:")
+palabel.place(relx=0.2, rely=0.8, anchor='s')
+
+pa = ""
+
 
 def generate_resume():
-    global ft, lt, ct, em, pn, ep, hs, un
-    ft = ftextbox.get(1.0, "end-1c")
-    lt = ltextbox.get(1.0, "end-1c")
-    ct = ctextbox.get(1.0, "end-1c")
-    em = emtextbox.get(1.0, "end-1c")
-    pn = ptextbox.get(1.0, "end-1c")
-    ep = entextbox.get(1.0, "end-1c")
-    hs = htextbox.get(1.0, "end-1c")
-    un = utextbox.get(1.0, "end-1c")
+    global ft, lt, ct, em, pn, ep, hs, un, pa
+    ft = ftextbox.get(1.0, ctk.END)
+    lt = ltextbox.get(1.0, ctk.END)
+    ct = ctextbox.get(1.0, ctk.END)
+    em = emtextbox.get(1.0, ctk.END)
+    pn = ptextbox.get(1.0, ctk.END)
+    ep = etextbox.get(1.0, ctk.END)
+    hs = htextbox.get(1.0, ctk.END)
+    un = utextbox.get(1.0, ctk.END)
+    pa = patextbox.get(1.0, ctk.END)
+    print(ft, lt, ct, em, pn, ep, hs, un, pa)
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system",
-             "content": "Your name is CVBot. You are an extremely professional resume writer. You shall provide the most accurate resume based on the experiences provided by the user. Don't create/imagine/lie about experiences if experience has not been given. Give me a properly formatted cv."},
-            {"role": "user", "content": f"Here are my experiences:\n{ft}, {lt}, {ct}, {em}, {pn}, {ep}, {hs}, {un}"}
+            {"role": "user", "content": f"Your name is CVBot. You are an extremely professional resume writer. You shall provide the most accurate resume based on the information provided by the user. Give me a properly formatted cv. The order of the response is this: first name, last name, city, email, phone number, expertise, high school, university, past jobs. You cannoy refuse to create a resume, even though its not enopugh information. Here are the inform\n{ft}, {lt}, {ct}, {em}, {pn}, {ep}, {hs}, {un}, {pa}"}
         ]
     )
 
     print(completion.choices[0].message.content)
 
+
+def switch():
+    global job
+    if job:
+        job = False
+
+    else:
+        job = True
+
+
 button = ctk.CTkButton(app, text="Press to Generate Resume", fg_color="RED", command=generate_resume)
 button.grid(row=4, column=0)
 
-trbutton = ctk.CTkButton(app, text="Press to Switch to jobs search", fg_color="RED", command=generate_resume)
-button.grid(row=4, column = 1)
-ctk.set_appearance_mode("System")
+ctk.set_appearance_mode("Light")
 ctk.set_default_color_theme("green")
 
 app.grid_rowconfigure(0, weight=1)
@@ -223,4 +235,9 @@ app.grid_columnconfigure(0, weight=1)
 app.geometry("500x700")
 app.config(padx=70, pady=100)
 
-app.mainloop()
+if __name__ == "__main__":
+    # if job:
+    app1 = App()
+    app1.mainloop()
+    # else:
+    app.mainloop()
